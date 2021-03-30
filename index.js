73be507a8df482bf92c6b4bf3768a0c84ea981df
ATBo <somewater_a1@hotmail.com>
2021-03-30 17:16:28 +0700

    Function login api with sheet generate pdf online

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const port = process.env.PORT || 3000;
 
 const book = require("./data/book.json");
 
+// axios
+const axios = require("axios");
+
 //Required package
 var pdf = require("pdf-creator-node");
 var fs = require("fs");
@@ -23,10 +26,29 @@ app.use(
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send(book);
+  res.send("Hello api from AT");
 });
 // ====
- 
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  console.log(req.body);
+  axios
+    .get(
+      "https://script.google.com/macros/s/AKfycbwp6gBjY1GFab1l9y9hQFCW3p7wqdCmzpbJlmChUEdP9APVDA/exec?path=/cer"
+    )
+    .then((result) => {
+      let re = result.data.items;
+      for (let i = 0; i < re.length; i++) {
+        const ele = re[i];
+        if (username == ele.username && password == ele.password) {
+          res.send(ele);
+        }
+      }
+      res.send("Not found");
+    });
+});
+
 app.post("/book", (req, res) => {
   book.push({ id: req.body.id++, name: req.body.name });
   res.json(req.body);
